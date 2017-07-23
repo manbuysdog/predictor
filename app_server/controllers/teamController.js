00e8df726087fe9e00075af2b50b8be89dc3735e
Rob <robwilliams.email@gmail.com>
2017-07-23 10:25:07 +0100

    -msome changes

--- app_server/controllers/teamController.js
+++ app_server/controllers/teamController.js
@@ -1,7 +1,14 @@
 
 const listTeams = function(req, res, next){
-  res.render('teams/list', {title: 'Teams'});
+  var teams = [
+    {name: "Arsenal"},
+    {name: "Chelsea"},
+    {name: "Spurs"}
+  ];
+
+  res.render('teams/list', {title: 'Teams', teams: teams});
 };
+
 const getTeam = function(req, res, next){
   res.render('teams/details', {title: 'Details', team: req.params.team});
 };
